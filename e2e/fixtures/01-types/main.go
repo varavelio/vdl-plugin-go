48170fe21059ca860faa40091c24f0809798ddef
Luis Eduardo <eduardo.devop@gmail.com>
2026-03-12 17:12:59 +0000

    fix: improve JSON assertion logic in tests for better error handling

--- e2e/fixtures/01-types/main.go
+++ e2e/fixtures/01-types/main.go
@@ -13,16 +13,21 @@ func assert(name string, anyStruct any, expectedJSON string) {
 	if err != nil {
 		panic(name + ": error marshaling struct: " + err.Error())
 	}
-	if string(marshaled) != expectedJSON {
-		panic(name + ": unexpected json output")
+
+	var actual any
+	if err := json.Unmarshal(marshaled, &actual); err != nil {
+		panic(name + ": error unmarshaling marshaled json: " + err.Error())
 	}
 
-	var unmarshaled any
-	if err := json.Unmarshal(marshaled, &unmarshaled); err != nil {
-		panic(name + ": error unmarshaling json: " + err.Error())
+	var expected any
+	if err := json.Unmarshal([]byte(expectedJSON), &expected); err != nil {
+		panic(name + ": error unmarshaling expected json: " + err.Error())
 	}
-	if reflect.DeepEqual(anyStruct, unmarshaled) {
-		panic(name + ": unmarshaled struct does not match original")
+
+	if !reflect.DeepEqual(actual, expected) {
+		fmt.Println("actual:", string(marshaled))
+		fmt.Println("expected:", expectedJSON)
+		panic(name + ": unexpected json output")
 	}
 }
 
@@ -46,7 +51,22 @@ func main() {
 		OptBoolField:     new(true),
 		OptDatetimeField: new(time.Now()),
 	}
-	expected = `{"stringField":"hello","intField":42,"floatField":3.14,"boolField":true,"datetimeField":"` + anyStruct.(gen.Primitives).DatetimeField.Format(time.RFC3339Nano) + `","optStringField":"hello","optIntField":42,"optFloatField":3.14,"optBoolField":true,"optDatetimeField":"` + anyStruct.(gen.Primitives).OptDatetimeField.Format(time.RFC3339Nano) + `"}`
+	expected = fmt.Sprintf(
+		`{
+			"stringField":"hello",
+			"intField":42,
+			"floatField":3.14,
+			"boolField":true,
+			"datetimeField":"%s",
+			"optStringField":"hello",
+			"optIntField":42,
+			"optFloatField":3.14,
+			"optBoolField":true,
+			"optDatetimeField":"%s"
+		}`,
+		anyStruct.(gen.Primitives).DatetimeField.Format(time.RFC3339Nano),
+		anyStruct.(gen.Primitives).OptDatetimeField.Format(time.RFC3339Nano),
+	)
 	assert("Primitives", anyStruct, expected)
 
 	fmt.Println("Test pass!")
