3f829250c5964e7873fd54dadfe1ecab60c9f927
Luis Eduardo <eduardo.devop@gmail.com>
2026-03-21 16:43:06 +0000

    docs: add detailed documentation for the entry point of the VDL Go plugin

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { definePlugin } from "@varavel/vdl-plugin-sdk";
 import { generate as generateOutput } from "./generate";
 
+/**
+ * Entry point for the VDL Go plugin.
+ *
+ * This file adapts the pure generator orchestration from `./generate` to the
+ * VDL Plugin SDK interface using `definePlugin`.
+ */
 export const generate = definePlugin((input) => generateOutput(input));
